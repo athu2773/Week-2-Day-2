6d42b61a4cf2c20d3d44cca71cda3074e6e12aea
athu2773 <atharva.saawant@gmail.com>
2024-11-21 12:55:58 +0530

    Added functions to find even and odd numbers

--- project.js
+++ project.js
@@ -1,1 +1,12 @@
 console.log("Welcome to the Simple JS Project!");
+
+function findEvenNumbers(arr) {
+    return arr.filter(num => num % 2 === 0);
+}
+
+function findOddNumbers(arr) {
+    return arr.filter(num => num % 2 !== 0);
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("Even numbers:", findEvenNumbers(numbers));
+console.log("Odd numbers:", findOddNumbers(numbers));
